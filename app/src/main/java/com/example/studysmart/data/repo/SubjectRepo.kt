0adb95090da87f97fecbc3ef6d3ed8744d47a36a
Jiahui Qing <jqin0020@student.monash.edu>
2025-10-11 19:45:33 +1100

    Attempted to complete SubjectRepo.kt

--- app/src/main/java/com/example/studysmart/data/repo/SubjectRepo.kt
+++ app/src/main/java/com/example/studysmart/data/repo/SubjectRepo.kt
@@ -5,11 +5,29 @@ import com.example.studysmart.data.local.AppDatabase
 import com.example.studysmart.domain.model.Subject
 import kotlinx.coroutines.flow.Flow
 import com.example.studysmart.data.local.dao.SubjectDao
+import com.example.studysmart.data.local.entity.SubjectEntity
 
 
-
-class subjectRepository (application: Application){
+class subjectRepository (application: Application) {
     private var subjectDao: SubjectDao = AppDatabase.getDatabase(application).subjectDao()
+
+    val observeSubjects: Flow<List<SubjectEntity>> = subjectDao.observeSubjects()
+
+    suspend fun listSubjects() {
+        subjectDao.listSubjects()
+    }
+
+    suspend fun getSubjectById(id: Long) {
+        subjectDao.get(id)
+    }
+
+    suspend fun upsert(e: SubjectEntity){
+        subjectDao.upsert(e)
+    }
+
+    suspend fun delete(id: Long){
+        subjectDao.delete(id)
+    }
 
 
 }
