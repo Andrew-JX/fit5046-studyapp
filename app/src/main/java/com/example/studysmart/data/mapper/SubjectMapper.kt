62b02244e88fdd2e8ea6aea11d1ed63dbaa470af
Jiahui Qing <jqin0020@student.monash.edu>
2025-10-12 00:40:40 +1100

    Implemented SubjectRepoRoomImpl.kt, Created SubjectMapper.kt to convert Subject and SubjectEntity

---
+++ app/src/main/java/com/example/studysmart/data/mapper/SubjectMapper.kt
@@ -0,0 +1,26 @@
+package com.example.studysmart.data.mapper
+
+
+import com.example.studysmart.data.local.entity.SubjectEntity
+import com.example.studysmart.domain.model.Subject
+import com.example.studysmart.presentation.theme.gradient1
+
+fun SubjectEntity.toDomain(): Subject {
+    val colors = Subject.subjectCardColors.getOrNull(colorKey) ?: Subject.subjectCardColors.first()
+    return Subject(
+        name = this.name,
+        goalHours = this.goalHours,
+        colors = colors,
+        subjectId = this.id.toInt()
+    )
+}
+
+fun Subject.toEntity(): SubjectEntity {
+    val colorKey = Subject.subjectCardColors.indexOf(colors.firstOrNull() ?: gradient1)
+    return SubjectEntity(
+        id = this.subjectId.toLong(),
+        name = this.name,
+        goalHours = this.goalHours,
+        colorKey = colorKey
+    )
+}
--- app/src/main/java/com/example/studysmart/data/repo/impl/SubjectRepoRoomImpl.kt
+++ app/src/main/java/com/example/studysmart/data/repo/impl/SubjectRepoRoomImpl.kt
@@ -1,4 +1,34 @@
 package com.example.studysmart.data.repo.impl
 
-class SubjectRepositoryImpl {
+import com.example.studysmart.data.local.dao.SubjectDao
+import com.example.studysmart.data.repo.SubjectRepo
+import com.example.studysmart.domain.model.Subject
+import com.example.studysmart.data.mapper.toDomain
+import com.example.studysmart.data.mapper.toEntity
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class SubjectRepoRoomImpl @Inject constructor(
+    private val dao: SubjectDao
+) : SubjectRepo {
+    override fun observeSubjects(): Flow<List<Subject>> {
+        return dao.observeSubjects().map { subjectEntities ->
+            subjectEntities.map { it.toDomain() }
+        }
+    }
+
+    override suspend fun listSubjects(): List<Subject> {
+        return dao.listSubjects().map { it.toDomain() }
+    }
+
+    override suspend fun getSubject(id: Long): Subject? = dao.get(id)?.toDomain()
+
+    override suspend fun upsertSubject(s: Subject): Long = dao.upsert(s.toEntity())
+
+    override suspend fun deleteSubject(id: Long) = dao.delete(id)
+
+
 }
