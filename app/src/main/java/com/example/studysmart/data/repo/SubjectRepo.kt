226707d5e2ade41e1ab34511b075d1541cad9aff
Jiahui Qing <jqin0020@student.monash.edu>
2025-10-11 23:28:48 +1100

    Revert correct version of SubjectRepo.kt

--- app/src/main/java/com/example/studysmart/data/repo/SubjectRepo.kt
+++ app/src/main/java/com/example/studysmart/data/repo/SubjectRepo.kt
@@ -1,33 +1,12 @@
 package com.example.studysmart.data.repo
 
-import android.app.Application
-import com.example.studysmart.data.local.AppDatabase
 import com.example.studysmart.domain.model.Subject
 import kotlinx.coroutines.flow.Flow
-import com.example.studysmart.data.local.dao.SubjectDao
-import com.example.studysmart.data.local.entity.SubjectEntity
 
-
-class subjectRepository (application: Application) {
-    private var subjectDao: SubjectDao = AppDatabase.getDatabase(application).subjectDao()
-
-    val observeSubjects: Flow<List<SubjectEntity>> = subjectDao.observeSubjects()
-
-    suspend fun listSubjects() {
-        subjectDao.listSubjects()
-    }
-
-    suspend fun getSubjectById(id: Long) {
-        subjectDao.get(id)
-    }
-
-    suspend fun upsert(e: SubjectEntity){
-        subjectDao.upsert(e)
-    }
-
-    suspend fun delete(id: Long){
-        subjectDao.delete(id)
-    }
-
-
+interface SubjectRepo {
+    fun observeSubjects(): Flow<List<Subject>>
+    suspend fun listSubjects(): List<Subject>
+    suspend fun getSubject(id: Long): Subject?
+    suspend fun upsertSubject(s: Subject): Long
+    suspend fun deleteSubject(id: Long)
 }
