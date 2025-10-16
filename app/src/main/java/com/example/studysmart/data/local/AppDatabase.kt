e6e3ba0aa38017ab86e7e8faa937dc5bd6faa865
MinyuJi <minyuj207@gmail.com>
2025-10-17 00:35:30 +1100

    Update the version of exportSchema

--- app/src/main/java/com/example/studysmart/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/studysmart/data/local/AppDatabase.kt
@@ -11,7 +11,7 @@ import com.example.studysmart.data.local.entity.*
         SessionEntity::class,
         ResourceEntity::class
     ],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
